e1cb82cb5705cbea3e7f87cd38c7c9d074f755b9
Sabyasachi Mondal <inmygod@hotmail.com>
2026-06-23 22:43:36 +0600

    Update purchaseStore.ts

--- src/stores/purchaseStore.ts
+++ src/stores/purchaseStore.ts
@@ -1,1 +1,78 @@
-export {};
+import { create } from "zustand";
+import { PurchaseInvoice } from "../types/PurchaseInvoice";
+
+interface PurchaseStore {
+  purchases: PurchaseInvoice[];
+
+  addPurchase: (
+    purchase: PurchaseInvoice
+  ) => void;
+
+  updatePurchase: (
+    id: string,
+    purchase: Partial<PurchaseInvoice>
+  ) => void;
+
+  archivePurchase: (
+    id: string
+  ) => void;
+
+  deletePurchase: (
+    id: string
+  ) => void;
+}
+
+export const usePurchaseStore =
+  create<PurchaseStore>((set) => ({
+    purchases: [],
+
+    addPurchase: (purchase) =>
+      set((state) => ({
+        purchases: [
+          ...state.purchases,
+          purchase,
+        ],
+      })),
+
+    updatePurchase: (
+      id,
+      updatedPurchase
+    ) =>
+      set((state) => ({
+        purchases:
+          state.purchases.map((purchase) =>
+            purchase.id === id
+              ? {
+                  ...purchase,
+                  ...updatedPurchase,
+                }
+              : purchase
+          ),
+      })),
+
+    archivePurchase: (id) =>
+      set((state) => ({
+        purchases:
+          state.purchases.map((purchase) =>
+            purchase.id === id
+              ? {
+                  ...purchase,
+                  archived: true,
+                }
+              : purchase
+          ),
+      })),
+
+    deletePurchase: (id) =>
+      set((state) => ({
+        purchases:
+          state.purchases.map((purchase) =>
+            purchase.id === id
+              ? {
+                  ...purchase,
+                  deleted: true,
+                }
+              : purchase
+          ),
+      })),
+  }));
