4935afb03fb3b46c4dfcd77aa972c0e95b4e4b46
Sabyasachi Mondal <inmygod@hotmail.com>
2026-06-24 07:52:30 +0600

    Update settingsStore.ts

--- src/stores/settingsStore.ts
+++ src/stores/settingsStore.ts
@@ -1,1 +1,42 @@
-export {};
+import { create } from "zustand";
+import { AppSettings } from "../types/AppSettings";
+
+interface SettingsStore {
+  settings: AppSettings;
+
+  updateSettings: (
+    settings: Partial<AppSettings>
+  ) => void;
+
+  resetSettings: () => void;
+}
+
+const defaultSettings: AppSettings = {
+  darkMode: false,
+
+  recycleBinRetentionDays: 30,
+
+  allowInvoiceEditing: true,
+
+  language: "bn",
+};
+
+export const useSettingsStore =
+  create<SettingsStore>((set) => ({
+    settings: defaultSettings,
+
+    updateSettings: (
+      updatedSettings
+    ) =>
+      set((state) => ({
+        settings: {
+          ...state.settings,
+          ...updatedSettings,
+        },
+      })),
+
+    resetSettings: () =>
+      set({
+        settings: defaultSettings,
+      }),
+  }));
