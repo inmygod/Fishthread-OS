c9b3331c2c67baf33e3327ed8e72f9b78d41d6b8
Sabyasachi Mondal <inmygod@hotmail.com>
2026-06-24 07:58:19 +0600

    Update DashboardPage.tsx

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,3 +1,23 @@
 export default function DashboardPage() {
-  return <h1>ড্যাশবোর্ড</h1>;
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>FishThread OS</h1>
+
+      <p>
+        মাছ ও চিংড়ি ব্যবসা পরিচালনা
+        সিস্টেম
+      </p>
+
+      <hr />
+
+      <h3>দ্রুত প্রবেশ</h3>
+
+      <ul>
+        <li>নতুন ক্রয়</li>
+        <li>নতুন বিক্রয়</li>
+        <li>গ্রাহক তালিকা</li>
+        <li>রিপোর্ট</li>
+      </ul>
+    </div>
+  );
 }
